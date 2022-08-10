a262c879f9800517b91d66108cd7d5a409b4a869
Stephen Gordon <stephen@twistlock.com>
2022-08-10 10:15:08 -0500

    Commit

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ provider "aws" {
     region = "us-east-1"
   }    
 }
+
+
+resource "aws_route53_record" "default" {
+
+  name    = "sample.prismacompute.site"
+  zone_id = "Z03463741RM0EQ9QMBK5W"
+  ttl     = "60"
+  type    = "A"
+  records = "8.8.8.8"
+}
