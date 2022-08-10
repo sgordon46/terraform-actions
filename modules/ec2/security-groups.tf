d7b8ae3b0df6ccfcaec260d2c71f1e90113a2e21
Stephen Gordon <stephen@twistlock.com>
2022-08-10 11:17:56 -0500

    Commit

--- modules/ec2/security-groups.tf
+++ modules/ec2/security-groups.tf
@@ -1,17 +1,9 @@
 ### SECURITY GROUPS
 
 resource "aws_security_group" "default" {
-  name        = "Console Traffic - ${terraform.workspace} - ${var.tl_short_name}"
-  description = "Console Traffic - ${terraform.workspace} - ${var.tl_short_name}"
+  name        = "VM Traffic -  ${var.tl_short_name}"
+  description = "VM Traffic -  ${var.tl_short_name}"
   vpc_id      = var.vpc
-
-  ingress {
-    description = "(Ingress)8081-8083"
-    from_port   = 8081
-    to_port     = 8083
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     description = "(Ingress)22"
@@ -31,34 +23,7 @@ resource "aws_security_group" "default" {
   }
 
   tags = {
-    Name = "Console Traffic - ${var.tl_short_name}"
-  }
-}
-
-
-resource "aws_security_group" "default-LB" {
-  name        = "ALB Traffic - ${terraform.workspace} -${var.tl_short_name}"
-  description = "ALB Traffic - ${terraform.workspace} -${var.tl_short_name}"
-  vpc_id      = var.vpc
-
-  ingress {
-    description = "(Ingress)443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description     = "(Egress)8081-8083"
-    from_port       = 8081
-    to_port         = 8083
-    protocol        = "tcp"
-    security_groups = [aws_security_group.default.id]
-  }
-
-  tags = {
-    Name = "ALB Traffic - ${var.tl_short_name}"
+    Name = "VM Traffic - ${var.tl_short_name}"
   }
 }
 
